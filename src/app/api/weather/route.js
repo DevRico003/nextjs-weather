f301ad782b6d6125a88eb5b44c34398c10f3b7d3
Enrico Carteciano <carteciano.enrico@gmail.com>
2023-10-04 18:15:10 +0200

    next_public

--- src/app/api/weather/route.js
+++ src/app/api/weather/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const address = searchParams.get("address");
   let url = "";
-  const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY
   if (address) {
     url =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
